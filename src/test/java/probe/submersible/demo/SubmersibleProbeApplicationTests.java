1117ff7029c2420536873eaec7358523d1e8cd06
Rishabh Kabra <tujiorg@gamil.com>
2025-11-11 14:24:05 +0530

    Initial commit

---
+++ src/test/java/probe/submersible/demo/SubmersibleProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package probe.submersible.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubmersibleProbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
